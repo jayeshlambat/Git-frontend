3a0ec3c0ad14d96977b3ade10b092a045b322f13
jayeshlambat <jayeshlambat02@gmail.com>
2024-11-17 16:19:59 +0530

    first commit

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -47,17 +47,17 @@ const Header = () => {
         <div className='w-full h-[10%] bg-white dark:bg-[#28252E] flex items-center justify-between p-4 sm:px-6 lg:px-10 dark:shadow-md'>
             <div className='flex items-center space-x-4'>
                 <img src="./assets/logo.png" alt="Gitignore logo" className='h-8 sm:h-10' />
-                <h1 className='text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white'><span className='text-[#F05033]'>Git</span>Shield</h1>
+                <h1 className='text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white'><span className='text-[#F05033]'>Git</span>Filter</h1>
             </div>
-            
+
             <div className='flex space-x-4'>
                 <span>
-                <a href="https://github.com/PiyushB2003/GitShield" target='_blank'>
-                    <Tooltip title="Source code">
-                        <button className='bg-[#E4EDED] hover:bg-[#c6cece] dark:text-white dark:bg-[#3e3947] dark:hover:bg-[#534d5f] p-2 rounded-full flex items-center justify-center'>
-                            <GitHubIcon />
-                        </button>
-                    </Tooltip>
+                    <a href="https://github.com/PiyushB2003/GitShield" target='_blank'>
+                        <Tooltip title="Source code">
+                            <button className='bg-[#E4EDED] hover:bg-[#c6cece] dark:text-white dark:bg-[#3e3947] dark:hover:bg-[#534d5f] p-2 rounded-full flex items-center justify-center'>
+                                <GitHubIcon />
+                            </button>
+                        </Tooltip>
                     </a>
                 </span>
                 <span>
